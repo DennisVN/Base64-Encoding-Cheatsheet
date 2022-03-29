43dcdedbbcd6cc6643e6dceabb15d4b1670ee4d4
DennisVN <78357110+DennisVN@users.noreply.github.com>
2022-03-29 09:59:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Base64Encoding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainText = "Faffiage !";
+            string base64String = "RmFmZmlhZ2UgIQ==";
+            Console.WriteLine(Base64Encode(plainText)); // Outputs : "RmFmZmlhZ2UgIQ=="
+            Console.WriteLine(Base64Decode(base64String)); // Outputs : "Faffiage !"
+
+
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
